a06b01ae6cd4b394d74991b8c00fa968f6590aff
jerahmeel <jerahmond@yahoo.com>
2013-02-18 22:09:12 +0800

    project added

---
+++ GitSample/GitSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitSample
+//
+//  Created by Jerahmeel Duran on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitSample/GitSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by Jerahmeel Duran on 2/18/13.
+//  Copyright (c) 2013 Nextsprout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
